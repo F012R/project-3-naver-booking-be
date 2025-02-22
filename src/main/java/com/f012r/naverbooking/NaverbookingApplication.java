0e70708c68b03f4e37553dc943dd6f644608b801
Narae Gwon <rnjs021031@gmail.com>
2025-02-22 23:51:20 +0900

    ✏️ Chore: 프로젝트 초기 세팅

---
+++ src/main/java/com/f012r/naverbooking/NaverbookingApplication.java
@@ -0,0 +1,13 @@
+package com.f012r.naverbooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaverbookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NaverbookingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/f012r/naverbooking/NaverbookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.f012r.naverbooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NaverbookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
