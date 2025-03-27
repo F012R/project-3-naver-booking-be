8f9602031d71a81647ca849e2a4f8b8a8beed102
KMJ <minj20@duksung.ac.kr>
2025-03-28 00:15:27 +0900

    ♻ Refactor: 제품 목록 조회 기능을 ProductsController.java로 통합 #11

--- src/main/java/com/f012r/naverbooking/domain/products/controller/ProductsController.java
+++ src/main/java/com/f012r/naverbooking/domain/products/controller/ProductsController.java
@@ -1,14 +1,13 @@
 package com.f012r.naverbooking.domain.products.controller;
 
 import com.f012r.naverbooking.domain.products.dto.ProductsResponseDTO;
+import com.f012r.naverbooking.domain.products.dto.ProductsInfoResponseDTO;
+import com.f012r.naverbooking.domain.products.service.ProductsInfoService;
 import com.f012r.naverbooking.domain.products.service.ProductsService;
 import com.f012r.naverbooking.global.common.ResponseCode;
 import com.f012r.naverbooking.global.common.ResponseDTO;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -17,15 +16,15 @@ import java.util.List;
 public class ProductsController {
 
     private final ProductsService productsService;
+    private final ProductsInfoService productsInfoService;
 
-    @Autowired
-    public ProductsController(ProductsService productsService) {
+    public ProductsController(ProductsService productsService, ProductsInfoService productsInfoService) {
         this.productsService = productsService;
+        this.productsInfoService = productsInfoService;
     }
 
     @GetMapping
     public ResponseEntity<ResponseDTO> getAllProducts() {
-
         List<ProductsResponseDTO.ProductDTO> productDTOs = productsService.getAllProducts();
         int totalCount = productDTOs.size();
 
@@ -39,5 +38,16 @@ public class ProductsController {
         return ResponseEntity
                 .status(ResponseCode.SUCCESS.getStatus().value())
                 .body(customResponse);
+    }
+
+    @GetMapping("/{displayInfoId}")
+    public ResponseEntity<ResponseDTO> getProductInfo(@PathVariable Integer displayInfoId) {
+        ProductsInfoResponseDTO productInfo = productsInfoService.getProductInfo(displayInfoId);
+
+        ResponseDTO<ProductsInfoResponseDTO> responseDTO = new ResponseDTO<>(ResponseCode.SUCCESS, productInfo);
+
+        return ResponseEntity
+                .status(ResponseCode.SUCCESS.getStatus().value())
+                .body(responseDTO);
     }
 }
